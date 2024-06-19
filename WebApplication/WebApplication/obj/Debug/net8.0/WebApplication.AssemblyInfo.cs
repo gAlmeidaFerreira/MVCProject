8eb81f1642294b3fea6ffa8537d7e65b9b5a9771
gAlmeidaFerreira <guialmeidaf@gmail.com>
2024-06-19 15:47:45 -0400

    initial .NET MVC solution added

--- WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89da66a891b7ad6e32e57bb0557d6be0245fb57b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55fbdc9c76c03d8d8ae4a96cd78d0d11fa2f71dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
