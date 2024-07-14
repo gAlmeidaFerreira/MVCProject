9ab6c3ac4cb5aa9ddd35d07000a6991f23aa4444
gAlmeidaFerreira <guialmeidaf@gmail.com>
2024-07-14 19:22:04 -0400

    implementing grouping search

--- WebApplicationMVC/WebApplicationMVC/Models/SalesRecord.cs
+++ WebApplicationMVC/WebApplicationMVC/Models/SalesRecord.cs
@@ -1,11 +1,16 @@
-﻿using WebApplicationMVC.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplicationMVC.Models.Enums;
 
 namespace WebApplicationMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DataType(DataType.Currency)]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
